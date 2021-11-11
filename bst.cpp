90ae53ec726204e6530238e16f79ced6c55bcc72
abhyudaya7 <rishiraj.abhyuday71899@gmail.com>
2021-11-11 22:16:29 +0530

    Added function to delete a node in BST

--- bst.cpp
+++ bst.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 struct Node {
     int data;
-    Node *left,*right;
+    Node *left, *right;  
     Node(int val) {
         data = val;
         left = NULL;
@@ -20,6 +20,58 @@ Node *insertBST(Node *root, int val) {
         root->left = insertBST(root->left, val);
     else 
         root->right = insertBST(root->right, val);
+    return root;
+}
+
+Node *getMinNode(Node *root) {
+    Node *current = root;
+
+    while (current and current->left) {
+        current = current->left;
+    }
+
+    return current;
+}
+
+Node *deleteNode(Node *root, int Key) {
+    if (root == NULL) 
+        return NULL;
+    
+    if (Key < root->data) {
+        root->left = deleteNode(root->left, Key);
+    }
+
+    else if (Key > root->data) {
+        root->right = deleteNode(root->right, Key);
+    }
+
+    else {
+        if (!root->left and !root->right) {
+            return NULL;
+        }
+
+        else if (!root->left) {
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        }
+
+        else if (!root->right) {
+            Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        // when both left and right child are not null
+        // find the indorder succesor -> the leftmost (smallest) node in the right subtree
+
+        Node *minNode = getMinNode(root->right);
+
+        root->data = minNode->data;
+
+        root->right = deleteNode(root->right, minNode->data);
+    }
+
     return root;
 }
 
@@ -40,5 +92,8 @@ int main() {
     }
     // print inorder
     inorder(root);
+    cout << "\n";
+    Node *newRoot = deleteNode(root, 1);
+    inorder(newRoot);
     return 0;
 }
